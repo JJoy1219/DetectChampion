2df4cc6dd6d5fe4c291edcf3fb8d1b219950b655
JJoy1219 <joeljoy1912@knights.ucf.edu>
2024-02-16 18:34:15 -0500

    init

---
+++ create_minimap.py
@@ -0,0 +1,73 @@
+from capture_screenshot import capture_screenshot
+import cv2
+from PIL import Image
+
+def create_minimap():
+    while True:
+        screenshot = capture_screenshot()
+        
+        h, w, c = screenshot.shape
+        minimap_ratio = 650 / 1080
+        minimap_x = int(minimap_ratio * h)
+
+        minimap_size = h - minimap_x
+        minimap = screenshot[-minimap_size:, -minimap_size:]
+        
+        #im = Image.fromarray(minimap)
+        #im.save('trialimage.png')
+
+        h, w, c = minimap.shape
+        #print(minimap.shape)
+        left = 0
+        right = 0
+        top = 0
+        bottom = 0
+
+        for x in range(w):
+            y = int(h / 2)
+            r, g, b = minimap[y][x]
+            
+            if r == 16 and g == 27 and b == 27:
+                left = x
+                break
+
+        for x in range(w - 1, 0, -1):
+            y = int(h / 2)
+            r, g, b = minimap[y][x]
+            
+            if r == 16 and g == 27 and b == 27:
+                right = x
+                break
+
+        for y in range(h):
+            x = int(w / 2)
+            r, g, b = minimap[y][x]
+            
+            if r == 16 and g == 27 and b == 27:
+                top = y
+                break
+
+        for y in range(h - 1, 0, -1):
+            x = int(w / 2)
+            r, g, b = minimap[y][x]
+            
+            if r == 16 and g == 27 and b == 27:
+                bottom = y
+                break
+
+        minimap = minimap[top - 1:bottom + 1, left - 1:right + 1]
+
+        h, w, c = minimap.shape
+        if h == 0 or w == 0:
+            print('Could not detect game map')
+            continue
+
+        minimap = cv2.resize(minimap, dsize=(256, 256), interpolation=cv2.INTER_LINEAR)
+        im = Image.fromarray(minimap)
+        return im
+        #im.save('trialimage1.png')
+
+        break
+
+    
+                
---
+++ run_prediction.py
@@ -0,0 +1,23 @@
+from ultralytics import YOLO
+from PIL import Image
+from create_minimap import create_minimap
+
+def predict_position(minimap_image):
+  model = YOLO("models/balanced-approach/weights/best.pt")
+  #buf = Image.open("minimapexample.png")
+  results = model.predict(minimap_image)
+  result = results[0]
+  output = []
+  for box in result.boxes:
+    x1, y1, x2, y2 = [
+      round(x) for x in box.xyxy[0].tolist()
+    ]
+    class_id = box.cls[0].item()
+    prob = round(box.conf[0].item(), 2)
+    x = ((x1 + x2)/512)*15000
+    y = (1 - ((y1 + y2)/512)) * 15000
+    output.append([
+      round(x), round(y), result.names[class_id], prob
+    ])
+  return output
+  [print(f"{name}: ({x},{y}) - {prob} chance") for x, y, name, prob in output]
